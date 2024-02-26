c650b0aa3f8f2ec0b80b0e6b99d1a9735cf83ef5
otmanon <otman.benchekroun@mail.utoronto.ca>
2024-02-26 15:16:48 -0500

    committed skinning.ipynb notebook

--- keyframe_animate.py
+++ keyframe_animate.py
@@ -12,7 +12,7 @@ def keyframe_animate(X, F,  A1, A2, interp, num_timesteps, fps=120):
     ps.set_give_focus_on_show(True)
     W = np.ones((X.shape[0], 1))
     for i in range(num_timesteps):
-        A = interp(A1, A2, i, num_timesteps)
+        A = interp(A1, A2, i/num_timesteps)
         V = linear_blend_skinning(X, W, A)
         ps_mesh.update_vertex_positions(V)
         ps.frame_tick()
--- slerp.py
+++ slerp.py
@@ -3,12 +3,12 @@ from rotate import rotate
 
 
 
-def slerp(r1, r2, s, t):
+def slerp(r1, r2, s):
     """
     Interpolates two affine matrices A1 and A2 with parameter s
     """
     r1 = np.array(r1)
     r2 = np.array(r2)
-    r = (1 - s/t) * r1 + s/t * r2
+    r = (1 - s) * r1 + s * r2
     R = rotate(r)
     return R
